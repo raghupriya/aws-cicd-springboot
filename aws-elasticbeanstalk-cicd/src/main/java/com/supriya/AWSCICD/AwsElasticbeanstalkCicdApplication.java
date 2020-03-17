6dfeeec60792c434a07ab497c06e29565b4d1dea
raghupriya <supriya.ramchandra@gmail.com>
2020-03-16 20:53:24 -0700

    s This is the first commit

---
+++ aws-elasticbeanstalk-cicd/src/main/java/com/supriya/AWSCICD/AwsElasticbeanstalkCicdApplication.java
@@ -0,0 +1,13 @@
+package com.supriya.AWSCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticbeanstalkCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticbeanstalkCicdApplication.class, args);
+	}
+
+}
